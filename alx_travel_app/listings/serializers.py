1b66df215cb1ae1e48a31d2bd67617220ea7075c
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-12-25 23:33:54 +0300

    serializers in listings/serializers.py for Listing and Booking models.

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,22 @@
-# Create serializers in listings/serializers.py for Listing and Booking models.
+from rest_framework import serializers
+from .models import Listing, Booking, Review
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'id', 'name', 'description', 'location', 'price_per_night',
+            'guest_capacity', 'amenities', 'host', 'created_at', 'updated_at'
+        ]
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    listing_name = serializers.ReadOnlyField(source='listing.name')
+    guest_name = serializers.ReadOnlyField(source='guest_name.username')
+
+    class Meta:
+        model = Booking
+        fields = [
+            'id', 'listing', 'listing_name', 'guest_name', 'total_price',
+            'status', 'start_date', 'end_date', 'created_at'
+        ]
